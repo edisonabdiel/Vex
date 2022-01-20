65db71dfcaa691f57959175a5a9481b1f1684b49
Edison Abdiel <edison.abdiel@soundcloud.com>
2022-01-20 18:55:25 +0100

    feat: Add sidebar

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -17,7 +17,7 @@ import logo from '../assets/camera_logo.png';
 const Home = () => {
 
     const [toggleSidebar, setToggleSidebar] = useState(false);
-    const [user, setUser] = useState();
+    const [user, setUser] = useState(null);
 
     console.log(user);
     
@@ -36,16 +36,24 @@ const Home = () => {
         <>
             <div className="flex md:flex-row h-screen transaction-height duration ease-out">
                 <div className="hidden md:flex h-screen flex-initial">
-                    <Sidebar />
+                    <Sidebar user={user && user}/>
                 </div>
                 <div className="flex md:hidden flex-row">
-                    <HiMenu fontSize={40} className="cursor-pointer text-white" onClick={() => setToggleSidebar(false)} />
+                    <HiMenu fontSize={40} className="cursor-pointer text-white" onClick={() => setToggleSidebar(true)} />
                     <Link to="/">
                         <img src={logo} alt="logo" className="h-12 w-12" />
                     </Link>
                     <Link to={`user-profile/${user?._id}`}>
                         <img src={user?.image} alt="user" className="h-12 w-12" />
                     </Link>
+                    {toggleSidebar && (
+                        <div className="fixed w-4/5 h-screen bg-white overflow-y-auto shadow-md z-10 animate-slide-in">
+                            <div className="absolute w-full flex justify-end items-center p-2">
+                                <AiFillCloseCircle fontSize={40} className="cursor-pointer text-black text-2xl animate-bounce" onClick={() => setToggleSidebar(false)} />
+                            </div>
+                            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+                        </div>
+                    )}
                 </div>
             </div>
         </>
