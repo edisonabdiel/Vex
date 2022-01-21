01c95ff6e04564b0fd8a8e3dfda697cc91d161fa
Edison Abdiel <edison.abdiel@soundcloud.com>
2022-01-21 20:46:19 +0100

    feat: Add Sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { NavLink, Link } from 'react-router-dom';
+import { RiHomeFill } from 'react-icons/ri';
+import { IoIosArrowForward } from 'react-icons/io';
 
-const Sidebar = () => {
+import logo from '../assets/camera_logo.png';
+
+const isActiveStyles = {}
+
+const isNotActiveStyles = "flex items-center px-5 gap-3 text-gray-500 font-bold py-2 px-4 rounded-lg shadow-lg hover:text-black transition-all duration-200"
+
+const Sidebar = ({ user, closeToggle }) => {
+
+    const handleCloseSidebar = () => {
+        if(closeToggle) closeToggle(false);
+     }
+
     return (
-        <div>
-            Sidebar
+        <div className="flex flex-col justify-between bg-gray-300 h-full overflow-y-scroll min-w-210 hide-scrollbar">
+            <div className="flex flex-col">
+                <Link to="/" className="flex px-5 gap-2 my-6 pt-1 w-190 item-center " onClick={handleCloseSidebar}>
+                <img src={logo} alt="logo" className="w-16" />
+                </Link>
+                <div className="flex flex-col gap-5">
+                    <NavLink to="/" className={({ isActive }) => isActive ? isActiveStyles : isNotActiveStyles }>
+                        <RiHomeFill className="text-black" />
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -18,9 +18,10 @@ const Home = () => {
 
     const [toggleSidebar, setToggleSidebar] = useState(false);
     const [user, setUser] = useState(null);
+    const scrollRef = useRef(null);
 
     console.log(user);
-    
+
     const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : null;
 
 
@@ -31,29 +32,41 @@ const Home = () => {
 
     }, []);
 
+    useEffect(() => {
+        scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+        scrollRef.current.scrollTo(0, 0);
+    }, []);
 
     return (
         <>
-            <div className="flex md:flex-row h-screen transaction-height duration ease-out">
+            <div className="flex bg-gray md:flex-row flex-col h-screen transition-height duration-75 ease-out">
                 <div className="hidden md:flex h-screen flex-initial">
-                    <Sidebar user={user && user}/>
+                    <Sidebar user={user && user} />
                 </div>
                 <div className="flex md:hidden flex-row">
-                    <HiMenu fontSize={40} className="cursor-pointer text-white" onClick={() => setToggleSidebar(true)} />
-                    <Link to="/">
-                        <img src={logo} alt="logo" className="h-12 w-12" />
-                    </Link>
-                    <Link to={`user-profile/${user?._id}`}>
-                        <img src={user?.image} alt="user" className="h-12 w-12" />
-                    </Link>
+                    <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+                        <HiMenu fontSize={40} className="cursor-pointer text-white" onClick={() => setToggleSidebar(true)} />
+                        <Link to="/">
+                            <img src={logo} alt="logo" className="w-28" />
+                        </Link>
+                        <Link to={`user-profile/${user?._id}`}>
+                            <img src="https://lh3.googleusercontent.com/a-/AOh14Gjj8jdCUEEUaoo3FqJP5AYdPlGX3qpLmXwJ3Kpy=s96-c" alt="user-pic" className="w-9 h-9 rounded-full " />
+                        </Link>
+                    </div>
                     {toggleSidebar && (
-                        <div className="fixed w-4/5 h-screen bg-white overflow-y-auto shadow-md z-10 animate-slide-in">
+                        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
                             <div className="absolute w-full flex justify-end items-center p-2">
-                                <AiFillCloseCircle fontSize={40} className="cursor-pointer text-black text-2xl animate-bounce" onClick={() => setToggleSidebar(false)} />
+                                <AiFillCloseCircle fontSize={30} className="cursor-pointer text-gray-900 text-2xl" onClick={() => setToggleSidebar(false)} />
                             </div>
-                            <Sidebar user={user && user} closeToggle={setToggleSidebar} />
+                            <Sidebar closeToggle={setToggleSidebar} user={user && user} />
                         </div>
                     )}
+                </div>
+                <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+                    <Routes>
+                        <Route path="/user-profile/:userId" element={<UserProfile />} />
+                        <Route path="/*" element={<Pins user={user && user} />} />
+                    </Routes>
                 </div>
             </div>
         </>
